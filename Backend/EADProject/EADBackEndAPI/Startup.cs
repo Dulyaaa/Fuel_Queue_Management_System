53dd0127cef12438199980dc5c6788b09012f054
AchiniIma <achiniimalka@gmail.com>
2022-10-24 11:32:38 +0530

    fuel stock changes

--- Backend/EADProject/EADBackEndAPI/Startup.cs
+++ Backend/EADProject/EADBackEndAPI/Startup.cs
@@ -37,6 +37,7 @@ namespace EADBackEndAPI
             services.AddSingleton<ShedService>();
             services.AddSingleton<UserService>();
             services.AddSingleton<UserfuelQueueService>();
+            services.AddSingleton<FuelStockService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
