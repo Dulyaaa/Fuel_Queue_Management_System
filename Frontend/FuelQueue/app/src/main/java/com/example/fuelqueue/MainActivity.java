3e56517361cf7d895cf9d21708ad1942fbad6dc9
Dulya Perera <it19236810@my.sliit.lk>
2022-10-26 22:03:18 +0530

    Extract strings

--- Frontend/FuelQueue/app/src/main/java/com/example/fuelqueue/MainActivity.java
+++ Frontend/FuelQueue/app/src/main/java/com/example/fuelqueue/MainActivity.java
@@ -1,28 +1,37 @@
 package com.example.fuelqueue;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.Objects;
 
+/**
+ * Splash screen initiate and go to login or register page
+ */
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Initiate UI of splash screen
+     *
+     * @param savedInstanceState Bundle
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_main);
+        /* Locate the UI in xml */
         Button btnLogin = findViewById(R.id.btn_login);
         Button btnRegister = findViewById(R.id.btn_register);
-
+        /* Navigate to login screen */
         btnLogin.setOnClickListener(view -> {
             Intent intent = new Intent(view.getContext(), LoginActivity.class);
             startActivity(intent);
         });
-
+        /* Navigate to register screen */
         btnRegister.setOnClickListener(view -> {
             Intent intent = new Intent(view.getContext(), RegisterActivity.class);
             startActivity(intent);
--- Frontend/FuelQueue/app/src/main/java/com/example/fuelqueue/RegisterActivity.java
+++ Frontend/FuelQueue/app/src/main/java/com/example/fuelqueue/RegisterActivity.java
@@ -1,26 +1,32 @@
 package com.example.fuelqueue;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.fuelqueue.screen.station_owner.OwnerFuelTypeListActivity;
-import com.example.fuelqueue.screen.user.FuelStationsListActivity;
 
 import java.util.Objects;
 
+/**
+ * Initiate the register screen
+ */
 public class RegisterActivity extends AppCompatActivity {
-
+    /**
+     * Initiate the UI
+     *
+     * @param savedInstanceState Bundle
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_register);
-
+        /* Locate UI in xml */
         Button btnLogin = (Button) findViewById(R.id.btn_register);
-
+        /* Navigate to next screen */
         btnLogin.setOnClickListener(view -> {
             Intent intent = new Intent(view.getContext(), OwnerFuelTypeListActivity.class);
             startActivity(intent);
