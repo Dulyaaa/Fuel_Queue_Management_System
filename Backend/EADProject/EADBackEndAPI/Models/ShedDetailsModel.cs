a9c2cf786d5e6111763a7f601e6ccfd3375a7749
Dulya Perera <it19236810@my.sliit.lk>
2022-10-28 16:13:41 +0530

    changed

--- Backend/EADProject/EADBackEndAPI/Models/ShedDetailsModel.cs
+++ Backend/EADProject/EADBackEndAPI/Models/ShedDetailsModel.cs
@@ -10,6 +10,7 @@ namespace EADBackEndAPI.Models
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
         public string City { get; set; }
+        public string UserId { get; set; }
         public string ShedName { get; set; }
         public string AvgTime { get; set; }
         public string QueueLength { get; set; }
